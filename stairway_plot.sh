d41489be9d853f19e90c6cc7b5bf27ee330b4cef
Anaisa Cajigas Gandia <acajigasgandia27@gmail.com>
2026-04-16 16:32:52 +0200

    Update stairway_plot.sh

--- stairway_plot.sh
+++ stairway_plot.sh
@@ -1,7 +1,7 @@
 cd $HOME/vicentei/population_genetics/demography/stairwayplot
 
 # now run all blueprints
-java -cp stairway_plot_es Stairbuilder CEI.realSFS.blueprint
+java -cp stairway_plot_es Stairbuilder East.realSFS.blueprint
 bash East.realSFS.blueprint.sh
 
 java -cp stairway_plot_es Stairbuilder West.realSFS.blueprint
